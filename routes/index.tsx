33574e34ca42331ebea85995b3158887ec9c3490
Jonathan Hudak <hudak.jonathan@gmail.com>
2022-12-26 19:52:43 -0800

    Setup fresh app

---
+++ routes/index.tsx
@@ -0,0 +1,20 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hudak</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <nav>
+          <a href="/">hudak.link</a>
+        </nav>
+        <main>
+          <h1 class="text-5xl">Welcome to hudak.link</h1>
+          <p>Explanation of what this is exactly is pending...</p>
+        </main>
+      </div>
+    </>
+  );
+}
