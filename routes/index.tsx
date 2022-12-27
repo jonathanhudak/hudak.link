e762e855763df61a367eca33a5e63f632b3c713d
Jonathan Hudak <hudak.jonathan@gmail.com>
2022-12-26 20:07:22 -0800

    Add dark mode

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,14 +6,16 @@ export default function Home() {
       <Head>
         <title>Hudak</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <nav>
-          <a href="/">hudak.link</a>
-        </nav>
-        <main>
-          <h1 class="text-5xl">Welcome to hudak.link</h1>
-          <p>Explanation of what this is exactly is pending...</p>
-        </main>
+      <div class="bg-white dark:bg-black dark:text-white min-h-screen w-full">
+        <div class="p-4 mx-auto max-w-screen-md">
+          <nav>
+            <a href="/">hudak.link</a>
+          </nav>
+          <main>
+            <h1 class="text-5xl">Welcome to hudak.link</h1>
+            <p>Explanation of what this is exactly is pending...</p>
+          </main>
+        </div>
       </div>
     </>
   );
